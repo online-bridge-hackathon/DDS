9f387c6424fdbc56eaa1df445c7f178483fec029
brewing1 <brauny_13@hotmail.com>
2020-05-23 21:48:04 +0200

    Added test query string

--- src/test_dds.py
+++ src/test_dds.py
@@ -18,26 +18,8 @@ from dds import DDS
 class TestDDS(unittest.TestCase):
 
     def test_dds_scores(self):
-
-        # To Do: Move this file to a 'test' directory
-        #        Check the entire returned JSON object.
-        #        Add more test deals.
-        #        Test for different values of max_threads.
-        #        Test for invalid input, e.g. too many cards, too few, duplicated card
-        #        Tweak input by exchanging an A and a K, make sure output changes to match.
-
-        deal_string = """
-            {"hands":
-                {"S":["D3", "C6", "DT", "D8", "DJ", "D6", "CA", "C3", "S2", "C2", "C4", "S9", "S7"],
-                 "W":["DA", "S4", "HT", "C5", "D4", "D7", "S6", "S3", "DK", "CT", "D2", "SK", "H8"],
-                 "N":["C7", "H6", "H7", "H9", "CJ", "SA", "S8", "SQ", "D5", "S5", "HK", "C8", "HA"],
-                 "E":["H2", "H5", "CQ", "D9", "H4", "ST", "HQ", "SJ", "HJ", "DQ", "H3", "C9", "CK"]}
-            }"""
-        deal_json = json.loads(deal_string)
-
-        # Disable 'String statement has no effect (pointless-string-statement)' warning
-        #pylint: disable=W0105 
         """
+        Manually tests DDS output for the following hand:
             S AQ85
             H AK976
             D 5
@@ -52,7 +34,22 @@ class TestDDS(unittest.TestCase):
             H
             D JT863
             C A6432
+
+        TODO:   Move this file to a 'test' directory
+                Check the entire returned JSON object.
+                Add more test deals.
+                Test for different values of max_threads.
+                Test for invalid input, e.g. too many cards, too few, duplicated card
+                Tweak input by exchanging an A and a K, make sure output changes to match.
         """
+        deal_string = """
+            {"hands":
+                {"S":["D3", "C6", "DT", "D8", "DJ", "D6", "CA", "C3", "S2", "C2", "C4", "S9", "S7"],
+                 "W":["DA", "S4", "HT", "C5", "D4", "D7", "S6", "S3", "DK", "CT", "D2", "SK", "H8"],
+                 "N":["C7", "H6", "H7", "H9", "CJ", "SA", "S8", "SQ", "D5", "S5", "HK", "C8", "HA"],
+                 "E":["H2", "H5", "CQ", "D9", "H4", "ST", "HQ", "SJ", "HJ", "DQ", "H3", "C9", "CK"]}
+            }"""
+        deal_json = json.loads(deal_string)
 
         dds = DDS(max_threads=2)
         dds_table = dds.calc_dd_table(deal_json['hands'])
