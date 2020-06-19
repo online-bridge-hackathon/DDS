d77bcd12af42ce7124260ca96db2a84aa39bcd95
Adam Wildavsky <adam@tameware.com>
2020-06-19 12:32:55 -0600

    Update test_dds.py
    Simplify - there's no need to wrap the 'hands' dict inside a 'deal' dict.

--- test/test_dds.py
+++ test/test_dds.py
@@ -40,17 +40,15 @@ class TestDDS(unittest.TestCase):
                 Tweak input by exchanging an A and a K, make sure output changes to match.
         """
         
-        deal = {"hands":
-                {
+        hands = {
                  "S":["D3", "C6", "DT", "D8", "DJ", "D6", "CA", "C3", "S2", "C2", "C4", "S9", "S7"],
                  "W":["DA", "S4", "HT", "C5", "D4", "D7", "S6", "S3", "DK", "CT", "D2", "SK", "H8"],
                  "N":["C7", "H6", "H7", "H9", "CJ", "SA", "S8", "SQ", "D5", "S5", "HK", "C8", "HA"],
                  "E":["H2", "H5", "CQ", "D9", "H4", "ST", "HQ", "SJ", "HJ", "DQ", "H3", "C9", "CK"]
                 }
-               }
 
         dds = DDS()
-        dds_table = dds.calc_dd_table(deal['hands'])
+        dds_table = dds.calc_dd_table(hands)
 
         self.assertEqual(8, dds_table['C']['S'], 'South can take 8 tricks with clubs as trump')
         self.assertEqual(6, dds_table['N']['E'], 'East can take 6 tricks at notrump')
