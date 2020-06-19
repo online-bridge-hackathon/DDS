a0618890300d030652c98f5ea486cc99f16eb045
Adam Wildavsky <adam@tameware.com>
2020-06-19 12:49:26 -0600

    More cleanup
    Reformat and remove trailing whitespace.

--- test/test_dds.py
+++ test/test_dds.py
@@ -39,13 +39,13 @@ class TestDDS(unittest.TestCase):
                 Test for invalid input, e.g. too many cards, too few, duplicated card
                 Tweak input by exchanging an A and a K, make sure output changes to match.
         """
-        
+
         hands = {
-                 "S":["D3", "C6", "DT", "D8", "DJ", "D6", "CA", "C3", "S2", "C2", "C4", "S9", "S7"],
-                 "W":["DA", "S4", "HT", "C5", "D4", "D7", "S6", "S3", "DK", "CT", "D2", "SK", "H8"],
-                 "N":["C7", "H6", "H7", "H9", "CJ", "SA", "S8", "SQ", "D5", "S5", "HK", "C8", "HA"],
-                 "E":["H2", "H5", "CQ", "D9", "H4", "ST", "HQ", "SJ", "HJ", "DQ", "H3", "C9", "CK"]
-                }
+            "S":["D3", "C6", "DT", "D8", "DJ", "D6", "CA", "C3", "S2", "C2", "C4", "S9", "S7"],
+            "W":["DA", "S4", "HT", "C5", "D4", "D7", "S6", "S3", "DK", "CT", "D2", "SK", "H8"],
+            "N":["C7", "H6", "H7", "H9", "CJ", "SA", "S8", "SQ", "D5", "S5", "HK", "C8", "HA"],
+            "E":["H2", "H5", "CQ", "D9", "H4", "ST", "HQ", "SJ", "HJ", "DQ", "H3", "C9", "CK"]
+        }
 
         dds = DDS()
         dds_table = dds.calc_dd_table(hands)
