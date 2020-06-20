18ec02f7b5d121e5282ef1654a0c6b77d5df1b6f
Adam Wildavsky <adam@tameware.com>
2020-06-20 09:49:51 -0600

    Update test_dds.py
    Added two new test cases.

--- test/test_dds.py
+++ test/test_dds.py
@@ -13,12 +13,24 @@ import unittest
 from test.utilities import nesw_to_dds_format
 from src.dds import DDS
 
+# So we can use multi-line strongs as comments:
+# pylint: disable=pointless-string-statement
+
 class TestDDS(unittest.TestCase):
 
     def test_dds_scores(self):
         """
-        Manually tests DDS output for the following deal:
+        Tests DDS output for a few specific deals.
 
+        TODO:   Test for different values of max_threads.
+                Test for invalid input, e.g. too many cards, too few, duplicated card.
+                Tweak input by exchanging an A and a K, make sure output changes to match.
+                Rotate the grand slam deal so that EW can take all the tricks.
+        """
+
+        dds = DDS()
+
+        """
             S AQ85
             H AK976
             D 5
@@ -33,12 +45,6 @@ class TestDDS(unittest.TestCase):
             H
             D JT863
             C A6432
-
-        TODO:   Check the entire returned JSON object.
-                Add more test deals.
-                Test for different values of max_threads.
-                Test for invalid input, e.g. too many cards, too few, duplicated card.
-                Tweak input by exchanging an A and a K, make sure output changes to match.
         """
 
         nesw = [
@@ -50,11 +56,81 @@ class TestDDS(unittest.TestCase):
 
         hands = nesw_to_dds_format(nesw)
 
-        dds = DDS()
         dds_table = dds.calc_dd_table(hands)
 
         self.assertEqual(8, dds_table['C']['S'], 'South can take 8 tricks with clubs as trump')
         self.assertEqual(6, dds_table['N']['E'], 'East can take 6 tricks at notrump')
+
+        """
+            S AKQJ
+            H AKQJ
+            D T98
+            C T9
+
+        S 76        5432
+        H 876       5432
+        D 7654      32
+        C 8765      432
+
+            S T98
+            H T9
+            D AKQJ
+            C AKQJ
+        """
+
+        nesw = [
+            "AKQJ.AKQJ.T98.T9",
+            "5432.5432.32.432",
+            "T98.T9.AKQJ.AKQJ",
+            "76.876.7654.8765"
+        ]
+
+        hands = nesw_to_dds_format(nesw)
+
+        dds_table = dds.calc_dd_table(hands)
+
+        for denomination in ['C', 'D', 'H', 'S', 'N']:
+            for declarer in ['N', 'S']:
+                self.assertEqual(13, dds_table[denomination][declarer],
+                                 "NS can take 13 tricks in any denomination.")
+            for declarer in ['E', 'W']:
+                self.assertEqual(0, dds_table[denomination][declarer],
+                                 "EW can take 0 tricks in any denomination.")
+
+        """
+        Unusual deal! Everyone makes 3N.
+        See: https://bridge.thomasoandrews.com/deals/everybody/
+
+            S QT9
+            H A8765432
+            D KJ
+            C -
+
+        S -         KJ
+        H KJ        -
+        D QT9       A8765432
+        C A8765432  QT9
+
+            S A8765432
+            H QT9
+            D -
+            C KJ
+        """
+
+        nesw = [
+            "QT9.A8765432.KJ.",
+            "KJ..A8765432.QT9",
+            "A8765432.QT9..KJ",
+            ".KJ.QT9.A8765432"
+        ]
+
+        hands = nesw_to_dds_format(nesw)
+
+        dds_table = dds.calc_dd_table(hands)
+
+        for declarer in ['N', 'E', 'S', 'W']:
+            self.assertEqual(9, dds_table['N'][declarer],
+                             "Every declarer can take 9 tricks at NT.")
 
 if __name__ == '__main__':
     unittest.main()
