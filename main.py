f37c7f0db682d1268c062dfbb4457fa237852cd7
Adam Wildavsky <adam@tameware.com>
2020-09-20 20:28:03 -0400

    Updated comments.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#   Copyright 2020 Adam Wildavsky and the Bridge Hackathon contributors
+#   Copyright 2020 Adam Wildavsky and the Bridge Hackathon contributors.
 #   Use of this source code is governed by an MIT-style
 #   license that can be found in the LICENSE file or at
 #   https://opensource.org/licenses/MIT
